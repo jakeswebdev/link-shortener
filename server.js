6f0823e5333321bbbf31c8168c0cfbe3669e0699
jakeswebdev <jakeswebdev@gmail.com>
2018-04-24 17:39:43 -0500

    Added more accurate OS detection.

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const bodyParser = require('body-parser');
 const os = require('os');
 const app = express();
 const path = require('path');
+const parser = require ('ua-parser-js');
 
 app.set('trust proxy',true);
 
 app.get('/',function(request,response){
+    
     let address = request.ip;
-    let userType = os.type;
-    let userPlatform = os.platform;
-    let userRelease = os.release;
-    let userArch = os.arch;
+    let os = parser(request.headers['user-agent']);
+    let software = os.ua;
+    let userOS = software.match(/\(([^)]+)\)/);
     let lenguage = request.headers["accept-language"];
     let lenguageArr = lenguage.split(",");
     let len = lenguageArr[0];
-    let obj = {"address":address,"software": userType +  " " + userRelease + ";" + " " + userPlatform + ", " + userArch,"lenguage":len};
+    let obj = {"address":address,"software":userOS[1],"lenguage":len};
     response.json(obj);
 })
 
